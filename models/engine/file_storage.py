b52cd6aeb378b11f4d00f689c20c550052e9c98a
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2023-06-09 12:49:23 +0300

    file storage changes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -51,13 +51,20 @@ class FileStorage:
         except FileNotFoundError:
             pass
         
-            def delete(self, obj=None):
-                """ delete obj from __objects if it’s inside
-                Args:
-                obj: given object"""
-            if obj2sctype:
-                name = type(object_).__name__
-                id = object0.id
-                keyToDel = "{}.{}".format(name, id)
-                if keyToDel in self.__objects:
-                    del self.__objects[keyToDel]
+    def delete(self, obj=None):
+        """ delete obj from __objects if it’s inside
+        Args:
+            obj: given object
+        """
+        
+        if obj2sctype:
+            name = type(object_).__name__
+            id = object0.id
+            keyToDel = "{}.{}".format(name, id)
+            if keyToDel in self.__objects:
+                del self.__objects[keyToDel]
+                    
+    def close(self):
+         """calling the reload method
+         """
+         self.reload()
