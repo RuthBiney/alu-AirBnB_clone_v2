362a1992ab3987e67e01e9acb91c2c988eefd279
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2023-06-23 00:45:00 +0300

    updates

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,20 +11,20 @@ env.hosts = ['54.160.162.167', '54.165.86.206']
 
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    if not exists(archive_path):
         return False
     try:
         file_n = archive_path.split("/")[-1]
         no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
+        path = "/data/web_static/releases/" + no_ext
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('mkdir -p {}{}/'.format(path))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path))
         run('rm /tmp/{}'.format(file_n))
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('ln -s {}{}/ /data/web_static/current'.format(path))
         return True
     except ReferenceError:
         return False
